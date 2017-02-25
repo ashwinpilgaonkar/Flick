d478478c16d84cb0ccd71365f5653ddc3d9c3dae
Ashwin Pilgaonkar <ashwinpilgaonkar@gmail.com>
2017-02-26 04:17:52 +0530

    Referenced UI Elements

---
+++ main.py
@@ -0,0 +1,73 @@
+import sys
+from PyQt5.QtWidgets import *
+from Voice.SpeechRecognition import listen
+from Voice.GoogleTTS import speak
+from Voice.GoogleNewsParser import retrieveNews
+import UI.MainWindow
+import threading
+from multiprocessing import Queue
+
+class MainWindow(QMainWindow, UI.MainWindow.Ui_MainWindow):
+
+    DropDownText=""
+
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+        #Initialize Gesture Page UI Elements
+        self.ReadGestureButton.clicked.connect(self.ReadGestureButton_OnClick)
+        self.CaptureGestureButton.clicked.connect(self.CaptureGestureButton_OnClick)
+        self.AddButton.clicked.connect(self.AddButton_OnClick)
+        self.ClearButton.clicked.connect(self.ClearButton_OnClick)
+        self.SaveButton.clicked.connect(self.SaveButton_OnClick)
+        self.FunctionDropdown.addItems(["A", "B", "C"])
+        self.FunctionDropdown.currentIndexChanged.connect(self.Dropdown_OnChange)
+
+        #initialize Voice Page UI Elements
+        self.TalkButton.clicked.connect(self.TalkButton_OnClick)
+
+    def ReadGestureButton_OnClick(self):
+        print("Read Gesture")
+        self.StatusLabel.setText("Reading...")
+
+    def CaptureGestureButton_OnClick(self):
+        print("Capture Gesture")
+        self.StatusLabel.setText("Captured!")
+
+    def AddButton_OnClick(self):
+        print("Add")
+
+    def ClearButton_OnClick(self):
+        print("Clear")
+
+    def SaveButton_OnClick(self):
+        print("Save")
+
+    def Dropdown_OnChange(self):
+        self.DropDownText=self.FunctionDropdown.currentText()
+
+    def TalkButton_OnClick(self):
+        #Convert listened speech to text
+        queue = Queue()
+        text = ""
+        self.VoiceLabel.setText("Listening...")
+        listen_thread = threading.Thread(target=listen)
+        listen_thread.start()
+
+        #text = queue.get()
+        #self.VoiceLabel.setText(text)
+
+        #Speak recognized text (STT)
+        if not listen_thread.is_alive():
+            news = retrieveNews(text)
+            speak(news)
+
+def main():
+    app = QApplication(sys.argv)
+    form = MainWindow()
+    form.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
