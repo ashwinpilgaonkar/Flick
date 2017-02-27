705dd0a6b992a127e020b0f832373849c52bf47c
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-02-28 00:25:07 +0530

    bluetooth feature added

--- Bluetooth/Bluetooth.py
+++ Bluetooth/Bluetooth.py
@@ -1,15 +1,23 @@
 import serial
-import time
 
 port = "/dev/tty.HC-05-DevB"
 bluetooth = serial.Serial(port,9600)
 print("Connected")
 bluetooth.flushInput()
+print("Readable: ")
+print(bluetooth.readable())
 
+print("Entering loop")
 while 1:
     input_data = bluetooth.readline()
-    print(input_data.decode())
-    time.sleep(0.1)
+    try:
+        try:
+            print(input_data.decode())
+            print('\n')
+        except:
+            pass
+    except serial.serialutil.SerialException:
+        break
 
 bluetooth.close()
 print("close")
