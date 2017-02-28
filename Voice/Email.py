da5c82cee5608653d90b119c7c68898d147c3fd6
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-02-28 23:50:34 +0530

    Email updated

--- Voice/Email.py
+++ Voice/Email.py
@@ -1,13 +1,14 @@
 import smtplib
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-print("smtp set")
-server.starttls()
-print("start ttls")
-server.login("priyanshubhatnagar1851996@gmail.com", "Dimpo@22")
-print("login complete")
+def mail(TO, SUB, CONTENT):
+    From = "priyanshubhatnagar1851996@gmail.com"
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    print("smtp set")
+    server.starttls()
+    print("start ttls")
+    server.login(From, "9825132939")
+    print("login complete")
 
-msg = "YOUR MESSAGE!"
-server.sendmail("priyanshubhatnagar1851996@gmail.com", "rashika2014@gmail.com", msg)
-print("Sent")
-server.quit()
+    server.sendmail(From, TO, CONTENT)
+    print("Sent")
+    server.quit()
