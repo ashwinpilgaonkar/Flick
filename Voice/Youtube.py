ad78dce885bf2077bb5ced6e994c8744bae97853
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-04-08 22:53:27 +0530

    Youtube fix

--- Voice/Youtube.py
+++ Voice/Youtube.py
@@ -2,13 +2,17 @@ import urllib.request
 import urllib.parse
 import re
 import webbrowser
+from Voice.GoogleTTS import speak
 
 def playYouTube(text):
-    query_string = urllib.parse.urlencode({"search_query" : text})
-    html_content = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
-    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-    url = "http://www.youtube.com/watch?v=" + search_results[0]
-    print(url)
-    webbrowser.open_new_tab(url)
+    try:
+        query_string = urllib.parse.urlencode({"search_query": text})
+        html_content = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
+        search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+        url = "http://www.youtube.com/watch?v=" + search_results[0]
+        print(url)
+        webbrowser.open_new_tab(url)
+    except:
+        speak("Sorry I did not get any result on Youtube.")
 
 #playYouTube("trap city")
