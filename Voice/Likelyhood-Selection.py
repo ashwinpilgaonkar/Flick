43b074358145d2f330dcae6b8b529513a1fd341b
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-04-11 18:20:28 +0530

    Semantic similarity- working 🎊

--- Voice/Likelyhood-Selection.py
+++ Voice/Likelyhood-Selection.py
@@ -1,8 +1,10 @@
 from textblob import TextBlob
 from Voice.SelectTask import *
+import numpy as np
+import math
 
 Responses = {"search" : ["who is donald trump", "what is cnc machine", "how to implement quicksort algorithm", "can you search how to remove stop words", "what is the weather today"] ,
-            "screenshot" : [] ,
+            "screenshot" : ["take a screenshot"] ,
             "type": ["can you type hello how are you"] ,
             "youtube" : ["I want to hear peaceful music", "play me a song starboy"] ,
             "news" : ["what is todays headlines", "tell me news about narendra modi"] ,
@@ -20,11 +22,8 @@ Match_Table = {
               }
 
 total_Num_Class = 7
+LowConfidence = 0.1
 
-
-
-def ask_Question():
-    return
 
 def pro_Class(key="search"):
     total_freq = 0
@@ -67,23 +66,82 @@ def cal_total_Pro(queryInterrogatives=[], queryVerbs=[], queryNouns=[], classNam
 
     return float(interrogatives_result) * float(verbs_result) * float(nouns_result) * float(pro_Class(className))
 
-def similiarity_eachClass_Response(blob, classname="search"):
-    nouns = GetNoun(blob)
-    for noun in nouns:
-        for eachSentences in Responses[classname]:
-            #if
-                if noun in GetNoun(eachSentences):
-                    break
+def similarity_Matrix(big_Union, text, row, column):
+    vec_list = np.zeros((row,column),dtype=np.float64)
 
-    return
+    i = 0
+    j = 0
 
-def match_Similarity_with_Resposes(blob):
-    interrogatives = GetInterrogatives(blob)
-    if len(interrogatives) > 0:
-        pass#similiarity_eachClass_Response(blob, "search")
-    else:
-        pass
-    return
+    for eachQueryWord in text.split():
+        for eachUnionWord in big_Union:
+            if eachQueryWord == eachUnionWord:
+                vec_list[i,j] = 1
+            else:
+                vec_list[i, j] = 0
+            j += 1
+        i += 1
+        j = 0
+
+    i = 0
+    j = 0
+
+    sim_vector = []
+    for eachUnionWord in big_Union:
+        max = 0
+        for eachQueryWord in text.split():
+            if vec_list[j,i] == 1:
+                max = 1
+                break
+            else:
+                similarity = SimilarityCheck(eachUnionWord, eachQueryWord)
+                if similarity > 0.5:
+                    vec_list[j,i] = similarity
+                else:
+                    vec_list[j,i] = 0
+
+                if max <= vec_list[j,i]:
+                    max = vec_list[j,i]
+
+            j+= 1
+
+        sim_vector.append(max)
+        i += 1
+        j = 0
+
+    print(sim_vector)
+    return sim_vector
+
+
+def match_Similarity_with_Resposes(className, queryText):
+    bag_Of_Words = []
+    for eachSentence in Responses[className]:
+        for eachWord in  eachSentence.split():
+            bag_Of_Words.append(eachWord)
+
+    big_Union = list(set().union(bag_Of_Words, queryText.split()))
+
+    row = len(queryText.split())
+    column = len(big_Union)
+    print("row: ", row, "column: ", column)
+
+    s1 = similarity_Matrix(big_Union, queryText,row, column)
+    row = len(bag_Of_Words)
+
+    classResponseText = ""
+    for eachWord in bag_Of_Words:
+        classResponseText += eachWord
+        classResponseText += " "
+
+    s2 = similarity_Matrix(big_Union,classResponseText,row, column)
+
+    dot_Product = np.dot(s1,s2)
+    magnitude_S1 = math.sqrt(sum(i**2 for i in s1))
+    magnitude_S2 = math.sqrt(sum(i**2 for i in s2))
+
+    cosine_Sim = dot_Product / (magnitude_S1 * magnitude_S2)
+
+    print("Cosine Similarity: ", cosine_Sim)
+    return cosine_Sim
 
 def remove_StopWords(text):
     StopWords = "a about above after again against all am an and any are aren't as at be because been before being below between both\
@@ -115,9 +173,6 @@ def bernoulli_Selection(text):
     nouns = GetNoun(blob)
     interrogatives = GetInterrogatives(blob)
 
-    if len(verbs) is 0:
-        verbs, nouns, interrogatives = match_Similarity_with_Resposes(blob)
-
     selection_list = [0,0,0,0,0,0,0]
 
     selection_list[0] = cal_total_Pro(interrogatives,verbs, nouns, "search")
@@ -128,10 +183,48 @@ def bernoulli_Selection(text):
     selection_list[5] = cal_total_Pro(interrogatives, verbs, nouns, "reminder")
     selection_list[6] = cal_total_Pro(interrogatives, verbs, nouns, "email")
 
-    print("Max Index: ", selection_list.index(max(selection_list)), "Probabilities: ", selection_list)
+    object1 = Rank()
+    object1.probability = max(selection_list)
+    object1.index = selection_list.index(object1.probability)
+
+    selection_list.insert(object1.index, 0)
+
+    object2 = Rank()
+    object2.probability = max(selection_list)
+    object2.index = selection_list.index(object2.probability)
+
+    print("Rank 1: ", object1.index, object1.probability)
+    print("Rank 2: ", object2.index, object2.probability)
+
+    if object1.probability - object2.probability <= LowConfidence:
+        similarity1 = match_Similarity_with_Resposes(ClassName[object1.index], text)
+        similarity2 = match_Similarity_with_Resposes(ClassName[object2.index], text)
+
+        if similarity1 >= similarity2:
+            pass
+        else: pass
+
+    else:
+        pass
+
+ClassName = {
+     0 : "search" ,
+     1 : "screenshot",
+     2 : "type" ,
+     3 : "youtube",
+     4 : "news",
+     5 : "reminder",
+     6 : "email"
+}
+
+
+class Rank:
+    index = 0
+    probability = 0
+
 
 def main():
-    bernoulli_Selection("I want to listen shape of you")
+    bernoulli_Selection("how to find dot product")
 
 
 if __name__ == '__main__':
